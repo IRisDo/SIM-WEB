caa925f72f01a533e56bd1f64e52d82f1be3a6a4
admin <shichibukai.sf@gmail.com>
2018-06-27 15:07:31 +0700

    Changed display

--- SIM/views.py
+++ SIM/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Operator, SIM
 # Create your views here.
 def sim_list(request):
-	return render(request,'SIM/sim_list.html',{})
+	list_viettel = SIM.objects.filter(operator = Operator.objects.get(Telcos='VIETTEL'))
+	list_mobifone = SIM.objects.filter(operator = Operator.objects.get(Telcos='MOBIFONE'))
+	list_vinaphone = SIM.objects.filter(operator = Operator.objects.get(Telcos='VINAPHONE'))
+	return render(request,'SIM/sim_list.html',{'list_viettel':list_viettel, 'list_vinaphone':list_vinaphone, 'list_mobifone':list_mobifone})
